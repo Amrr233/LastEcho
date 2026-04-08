472bccc7ebb2dd1b4162fa6eaab8e11f88d832b2
AHTX <abdullahhazem330@gmail.com>
2026-04-08 21:23:10 +0200

    inventory
    Added a toolbar with 6 slots and a selector

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -1,18 +1,19 @@
 #include "inventory.h"
-#include "inventory.h"
+
 void inventory::invt_init(float W, float H) {
     // 1. تحميل صورة الشريط (الـ 6 خانات)
-    invTex.loadFromFile("assets/gameplay/test.png");
+    invTex.loadFromFile("assets/gameplay/inventory_bar.png");
     invBar.setTexture(invTex);
 
     // 2. تظبيط مكان الشريط (تحت في النص)
     invBar.setOrigin(invTex.getSize().x / 2.0f, invTex.getSize().y);
-    invBar.setPosition(500.f,  500.0f); // بعيد عن الحافة التحتانية بـ 10 بكسل
+    invBar.setPosition(SCREEN_W/2,  SCREEN_H - 10.f); // بعيد عن الحافة التحتانية بـ 10 بكسل
+    invBar.setScale(0.5f, 0.5f);
 
     // 3. تظبيط شكل المربع اللي بيحدد الخانة
-    selector.setSize(sf::Vector2f(50.0f, 50.0f)); // مقاس المربع (عدله حسب مقاس خانتك)
+    selector.setSize(sf::Vector2f(60.0f, 60.0f)); // مقاس المربع (عدله حسب مقاس خانتك)
     selector.setFillColor(sf::Color::Transparent);
-    selector.setOutlineColor(sf::Color::Yellow);
+    selector.setOutlineColor(sf::Color(212, 181, 125));
     selector.setOutlineThickness(3);
 }
 void inventory::invt_update(sf::RenderWindow& window, AppState& currentState) {
@@ -27,7 +28,7 @@ void inventory::invt_update(sf::RenderWindow& window, AppState& currentState) {
 
     // تحديث مكان المربع الأصفر بناءً على الخانة المختارة
     float startX = invBar.getPosition().x - (invTex.getSize().x / 2.0f) + 5.0f;
-    selector.setPosition(startX + (selectedSlot * 55.0f), invBar.getPosition().y - 55.0f);
+    selector.setPosition(startX +263.f + (selectedSlot * 71.0f), invBar.getPosition().y - 100.0f);
 }
 void inventory::invt_draw(sf::RenderWindow& window) {
     // ... رسم البوز واللوحة ...
@@ -35,4 +36,5 @@ void inventory::invt_draw(sf::RenderWindow& window) {
     // رسم الانفنتوري (بتترسم دايماً)
     window.draw(invBar);
     window.draw(selector);
+
 }
