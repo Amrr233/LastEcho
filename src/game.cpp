84bebef18cff1e16e954569d86676dae27ea91d3
AHTX <abdullahhazem330@gmail.com>
2026-04-09 01:50:29 +0200

    pause control

--- src/game.cpp
+++ src/game.cpp
@@ -13,6 +13,7 @@ void Game::init(float W, float H) {
     // 3. تحميل اللوحة الخشبية (اللي شيلنا منها الزراير)
     pauseMenuTex.loadFromFile("assets/settings/pauseMenu.png");
     pauseMenuBG.setTexture(pauseMenuTex);
+    pauseMenuBG.setScale(2.f, 2.f);
     // سنترة اللوحة في نص الشاشة
     pauseMenuBG.setOrigin(pauseMenuTex.getSize().x / 2.f, pauseMenuTex.getSize().y / 2.f);
     pauseMenuBG.setPosition(W / 2.f, H / 2.f);
@@ -22,28 +23,25 @@ void Game::update(sf::RenderWindow& window, AppState& currentState) {
     sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
 
     // منطق زرار البوز
-    if (pauseBtn.getGlobalBounds().contains(mousePos) && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
         isPaused = !isPaused;
         sf::sleep(sf::milliseconds(250)); // عشان ميفضلش يلقط الضغطة كذا مرة
-    }
-
-    // لو اللعبة مش واقفة، حدث الحركة والوحوش
-    if (!isPaused) {
-        // player.update();
-        // enemies.update();
-    }
-}
-
-void Game::draw(sf::RenderWindow& window) {
-    // رسم اللعبة الأساسية دايماً
-    // player.draw(window);
-
-    // رسم زرار البوز
-    window.draw(pauseBtn);
-
-    // لو واخدين بوز، ارسم اللوحة الخشبية "فوق" كل حاجة
-    if (isPaused) {
-        window.draw(pauseMenuBG);
-        // هنا ممكن ترسم نصوص زي "Paused" أو زراير الرجوع
+        // لو اللعبة مش واقفة، حدث الحركة والوحوش
+        if (!isPaused) {
+            // player.update();
+            // enemies.update();
+        }
     }
 }
+    void Game::draw(sf::RenderWindow& window) {
+        // رسم اللعبة الأساسية دايماً
+        // player.draw(window);
+
+        // رسم زرار البوز
+
+        // لو واخدين بوز، ارسم اللوحة الخشبية "فوق" كل حاجة
+        if (isPaused) {
+            window.draw(pauseMenuBG);
+            // هنا ممكن ترسم نصوص زي "Paused" أو زراير الرجوع
+        }
+    }
