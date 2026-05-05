056f5824b73d685cac6b8a4328e3da3b232ebd45
its.saged <sagedahmed077@gmail.com>
2026-05-05 09:22:37 +0300

    Update DialogueManager.h

--- src/DialogueManager.h
+++ src/DialogueManager.h
@@ -3,10 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 
-<<<<<<< HEAD
-=======
 
->>>>>>> ed506049a4b519d1ddad2caa3a10bfc20004137b
 void initDialogue();
 void startDialogue(std::string name,std::string messages[],int count,sf::Texture& avatarTexture);
 void nextLine();
